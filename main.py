60e3ed82b3f6f979331d011c1cc855adcb810a85
lamerork <65411132+lamerork@users.noreply.github.com>
2023-05-24 11:25:50 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+
+urls = ['https://wttr.in/Шереметьево?nTqM&lang=ru', 
+        'https://wttr.in/Лондон?nTqM&lang=ru',
+        'https://wttr.in/Череповец?nTqM&lang=ru']
+
+for url in urls:
+    response = requests.get(url)
+    response.raise_for_status()
+    print(response.text)
