e4b0cbff50367d9ca4202d983a888e7be1239413
Anton Ozerov <lamerork@gmail.com>
2023-05-26 14:28:19 +0300

    Поправил ошибку

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import requests
 
 def get_weather():
 
-        urls = ['https://wttr.in/Шереметьево?nTqM&lang=ru', 
-                'https://wttr.in/Лондон?nTqM&lang=ru',
-                'https://wttr.in/Череповец?nTqM&lang=ru']
+        cities = ['Шереметьево',
+                'Лондон',
+                'Череповец']
+        params_dict = {'nTqM': '',
+                  'lang': 'ru'}
 
-        for url in urls:
-        response = requests.get(url)
-        response.raise_for_status()
-        print(response.text)
+        url = 'https://wttr.in/'
 
+        for city in cities:
+                response = requests.get(url + city, params=params_dict)
+                response.raise_for_status()
+                print(response.text)
+                print(response.url)
+
+if __name__ == '__main__':
+        get_weather()
